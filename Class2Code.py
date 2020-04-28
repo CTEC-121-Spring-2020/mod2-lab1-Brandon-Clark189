6be61fc10a910f391ecdd0e09968640c1fb8c9a1
Malachite189 <45154753+Malachite189@users.noreply.github.com>
2020-04-28 09:37:58 -0700

    Added class code, it should be functional.

---
+++ Class2Code.py
@@ -0,0 +1,49 @@
+"""
+CTEC 121
+<Brandon Norton>
+<Class 2 Lab code>
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    '''
+    A = 2
+    print("\n", A, "\n")
+    a = 22
+    print(a)
+    print()
+
+    AAA = 2
+    print(AAA)
+    
+    spam = 222
+    print()
+    print(spam)
+    print()
+    
+
+#expression
+#print(2)
+    print(3 - 1)
+    print(2 + ((3 - 1) / 2))
+    print(5/3)
+    print(5//3)
+    print(5%3)
+    '''
+
+    print()
+    print("The answer is:", 3 + 4, sep="-", end="\t")
+    print("end")
+    print()
+
+    for i in range(5):
+        print(i, end="\t")
+        print()
+
+main()  
